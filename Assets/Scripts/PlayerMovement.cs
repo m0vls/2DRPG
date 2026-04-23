c2b5e899187b1f1c8a8f16b77cc14514534cc6f0
m0vls <rakutin.vm@gmail.com>
2026-04-24 02:13:05 +0500

    updating packages

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,63 @@
+using Mirror;
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class PlayerMovement : MonoBehaviour
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : NetworkBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float moveSpeed = 5f;
+
+    private Rigidbody2D rb;
+    private InputSystem_Actions inputActions;
+    private Vector2 currentInput;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = 0;
+        inputActions = new InputSystem_Actions();
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void OnStartLocalPlayer()
     {
-        
+        inputActions.Player.Enable();
+    }
+
+    public void OnDisable()
+    {
+        inputActions.Player.Disable();
+    }
+
+    private void Update()
+    {
+        if (!isLocalPlayer) return;
+        currentInput = inputActions.Player.Move.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (isLocalPlayer)
+        {
+            ApplyMovement(currentInput);
+        }
+    }
+
+    private void ApplyMovement(Vector2 direction)
+    {
+        Vector2 velocity = direction.normalized * moveSpeed;
+        rb.linearVelocity = velocity;
+
+        if (!authority)
+        {
+            CmdSendInput(velocity);
+        }
+    }
+
+    [Command]
+    private void CmdSendInput(Vector2 velocity)
+    {
+        // сервер рассчитывает движение
+        rb.linearVelocity = velocity;
     }
 }
